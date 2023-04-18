94e7727dcf97b45918230c9aa92e458df6640391
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2023-04-17 23:10:49 -0500

    first cmd ping and add rest func

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+const { BOT_TOKEN, CLIENT_ID } = require('./config.json');
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
 const { Client, GatewayIntentBits } = require('discord.js');
-const { BOT_TOKEN } = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
